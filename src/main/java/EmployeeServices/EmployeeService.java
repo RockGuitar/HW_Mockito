9c1828d7d47eea6e8332d2d1438c932f28e6c6c3
Никита <oleinik.nikita96@gmail.com>
2022-04-07 21:30:44 +0300

    Initial commit

---
+++ src/main/java/EmployeeServices/EmployeeService.java
@@ -0,0 +1,18 @@
+package EmployeeServices;
+
+import EmployeeClass.Employee;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public interface EmployeeService {
+    Map<String, Employee> employeeMap = new HashMap<>();
+
+    void addEmployee ( String firstName, String lastName,int employeeDepartment, int employeePayment);
+
+    void removeEmployee ( String firstName, String lastName);
+
+    void findEmployee ( String firstName, String lastName);
+
+    Map<String, Employee> getEmployeeMap();
+}
